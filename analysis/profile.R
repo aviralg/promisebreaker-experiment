d607378dca12eb1bd3836f5ec17abad3f7edb845
Aviral Goel <goel.aviral@gmail.com>
2021-04-15 14:28:28 -0400

    Add analysis for indirect effects

--- analysis/profile.R
+++ analysis/profile.R
@@ -1103,6 +1103,53 @@ summarize_argument_type <- function(output) {
     list(argument_type = argument_type)
 }
 
+################################################################################
+## INDIRECT EFFECTS
+################################################################################
+
+reduce_indirect_effects <- function(data) {
+
+    arguments <- data$output$arguments
+    functions <- select(data$output$functions, fun_id, qual_name, anonymous)
+    effects <- data$output$effects
+
+    arguments <-
+        arguments %>%
+        filter(vararg == 0 & missing == 0) %>%
+        select(arg_id, fun_id, formal_pos, force_pos, arg_type, expr_type, val_type)
+
+    str(effects)
+
+    indirect_effects <-
+        effects %>%
+        filter(!is.na(source_fun_id)) %>%
+        left_join(functions, by = c("source_fun_id" = "fun_id"))%>%
+        filter(is.na(anonymous) | !anonymous) %>%
+        left_join(arguments, by = c("source_arg_id" = "arg_id")) %>%
+        select(-source_arg_id, -source_fun_id, -source_call_id, -anonymous) %>%
+        rename(source_qual_name = qual_name,
+               source_self_effect_seq = self_effect_seq,
+               source_effect_seq = effect_seq,
+               source_force_depth = force_depth,
+               source_default = default,
+               source_arg_type = arg_type,
+               source_expr_type = expr_type) %>%
+        left_join(functions, by = "fun_id") %>%
+        filter(is.na(anonymous) | !anonymous) %>%
+        left_join(calls, by = "call_id") %>%
+        left_join(arguments, by = "arg_id") %>%
+        select(-fun_id, -call_id, -anonymous) %>%
+        count(type, transitive, source_formal_pos, formal_pos,
+              source_qual_name, source_force_depth, source_default,
+              source_self_effect_seq, source_effect_seq, qual_name,
+              force_depth, default, self_effect_seq, effect_seq,
+              source_arg_type, source_expr_type, arg_type, expr_type, name = "operation_count")
+
+    str(indirect_effects)
+
+    list(indirect_effects = indirect_effects)
+}
+
 
 main()
 
